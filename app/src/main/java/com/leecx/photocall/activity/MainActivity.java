891626e20fd8150a49ae51abf97a4feab4599145
Mcflurry <824575210@qq.com>
2019-05-11 19:42:16 +0800

    tabLayout和tooBar功能完成

--- app/src/main/java/com/leecx/photocall/activity/MainActivity.java
+++ app/src/main/java/com/leecx/photocall/activity/MainActivity.java
@@ -5,21 +5,24 @@ import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.Button;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.leecx.photocall.R;
 import com.leecx.photocall.dao.DBManager;
@@ -31,138 +34,137 @@ import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends Activity {
-    ViewPager pager = null;
+    private ViewPager pager = null;
 
-    DBManager db = null;
-    Map<Integer, People> map = new HashMap<>();
-    List<View> pages = new ArrayList<>();
-
+    private DBManager db = null;
+    private Map<Integer, People> map = new HashMap<>();
+    private List<View> pages = new ArrayList<>();
     private boolean firstLauch = true;
+    private Toolbar toolbar = null;
+    private TabLayout mytab;
+
+    private List<String> mTabTitles;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE); // 注意顺序
         setContentView(R.layout.activity_main);
-        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,      // 注意顺序
-                R.layout.title);
-
-
-        Button btnAdd = (Button) findViewById(R.id.btnAdd);
-        Button btnUpdate = (Button) findViewById(R.id.btnUpdate);
-
-
-        LayoutInflater inflater = getLayoutInflater();
-        View view = inflater.inflate(R.layout.photo_view, null);
-        view.setBackgroundColor((getResources().getColor(R.color.colorTitleBar)));
-//        LinearLayout linearLayout = view.findViewById(R.id.titleLayout);
-//        linearLayout.setBackgroundResource(R.color.colorTitleBar);
-
-//        try {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//                LayoutInflater inflater = getLayoutInflater();
-//                Window window = getWindow();
-//                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-//                window.setStatusBarColor(getResources().getColor(R.color.colorPrimary));
-//                //底部导航栏
-//                //window.setNavigationBarColor(activity.getResources().getColor(colorResId));
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
 
         pager = (ViewPager) findViewById(R.id.viewPager);
+        toolbar = (Toolbar) findViewById(R.id.mainTb);
+        mytab = (TabLayout) findViewById(R.id.mytab);
         db = new DBManager(this);
 
-
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, ContactsAddFormActivity.class);
-                intent.putExtra("action", "add");
-                startActivity(intent);
-            }
-        });
-
-
-        btnUpdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (pages.size() <= 0) {
-                    return;
-                }
-                int position = pager.getCurrentItem();
-                View view = pages.get(position);
-
-                Integer id = getViewTag(view);
-                if (id == null) {
-                    return;
-                }
-
-                People people = map.get(id);
-
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this, ContactsAddFormActivity.class);
-                intent.putExtra("action", "update");
-                intent.putExtra("id", String.valueOf(people.getId()));
-                intent.putExtra("name", people.getName());
-                intent.putExtra("phoneNum", people.getPhoneNum());
-                intent.putExtra("photoPath", people.getPhotoPath());
-                intent.putExtra("rawContactId", people.getRawContactId());
-                startActivity(intent);
-            }
-        });
-
-
-        Button btnDelete = (Button) findViewById(R.id.btnDelete);
-        btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (pages.size() <= 0) {
-                    return;
-                }
-                int position = pager.getCurrentItem();
-                View view = pages.get(position);
-
-                Integer id = getViewTag(view);
-                if (id == null) {
-                    return;
-                }
-
-                final String idStr = String.valueOf(id);
-                People people = map.get(id);
-                final long rawContactId = people.getRawContactId();
-
-                AlertDialog confirmDialog = new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("是否删除")//标题
-                        .setIcon(R.mipmap.ic_launcher)//图标
-                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                db.deleteById(idStr);
-                                deleteSysContacts(rawContactId);
-                                restartActivity(MainActivity.this);
-                            }
-                        })
-                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                return;
-                            }
-                        })
-                        .create();
-                confirmDialog.show();
-            }
-        });
+        toolbar.inflateMenu(R.menu.main_toolbar_menu);
 
         showAllContacts();
+        bingBtnEvent();
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                Window window = getWindow();
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(getResources().getColor(R.color.colorTitleBar));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 
-    public void deleteSysContacts(long rawContactId){
+
+    private void toAddPeople() {
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, ContactsAddFormActivity.class);
+        intent.putExtra("action", "add");
+        startActivity(intent);
+    }
+
+    private void toUpdatePeople() {
+        if (pages.size() <= 0) {
+            return;
+        }
+        int position = pager.getCurrentItem();
+        View view = pages.get(position);
+
+        Integer id = getViewTag(view);
+        if (id == null) {
+            return;
+        }
+
+        People people = map.get(id);
+
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, ContactsAddFormActivity.class);
+        intent.putExtra("action", "update");
+        intent.putExtra("id", String.valueOf(people.getId()));
+        intent.putExtra("name", people.getName());
+        intent.putExtra("phoneNum", people.getPhoneNum());
+        intent.putExtra("photoPath", people.getPhotoPath());
+        intent.putExtra("rawContactId", people.getRawContactId());
+        startActivity(intent);
+    }
+
+    private void toDeletePeople() {
+        if (pages.size() <= 0) {
+            return;
+        }
+        int position = pager.getCurrentItem();
+        View view = pages.get(position);
+
+        Integer id = getViewTag(view);
+        if (id == null) {
+            return;
+        }
+
+        final String idStr = String.valueOf(id);
+        People people = map.get(id);
+        final long rawContactId = people.getRawContactId();
+
+        AlertDialog confirmDialog = new AlertDialog.Builder(MainActivity.this)
+                .setTitle("是否删除")//标题
+                .setIcon(R.mipmap.ic_launcher)//图标
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        db.deleteById(idStr);
+                        deleteSysContacts(rawContactId);
+                        restartActivity(MainActivity.this);
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        return;
+                    }
+                })
+                .create();
+        confirmDialog.show();
+    }
+
+    private void bingBtnEvent() {
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_add:
+                        toAddPeople();
+                        break;
+                    case R.id.action_update:
+                        toUpdatePeople();
+                        break;
+                    case R.id.action_delete:
+                        toDeletePeople();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    public void deleteSysContacts(long rawContactId) {
         //根据姓名求id
         ContentResolver resolver = this.getBaseContext().getContentResolver();
 
@@ -197,9 +199,12 @@ public class MainActivity extends Activity {
         }
 
         LayoutInflater inflater = getLayoutInflater();
+        mTabTitles = new ArrayList<>();
 
         for (People c : peopleList) {
             map.put(c.getId(), c);
+            mytab.addTab(mytab.newTab().setText(c.getName()));
+            mTabTitles.add(c.getName());
 
             Uri uri = Uri.parse("file://" + c.getPhotoPath());
             if (uri == null) {
@@ -207,32 +212,16 @@ public class MainActivity extends Activity {
             }
 
 
-
             View view = inflater.inflate(R.layout.photo_view, null);
             view.setTag(c.getId());
             ImageView imageView = view.findViewById(R.id.imgViewMain);
+            TextView phoneText = view.findViewById(R.id.phoneText);
+            phoneText.setText(c.getPhoneNum());
 
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
                 imageView.setImageBitmap(bitmap);
                 imageView.setTag(c.getId());
-//                imageView.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        Integer id  = getViewTag(v);
-//                        if (id == null) {
-//                            return;
-//                        }
-//
-//                        People contacts = map.get(id);
-//                        if (contacts == null) {
-//                            return;
-//                        }
-//
-//                        String phoneNum = contacts.getPhoneNum();
-//                        callPhone(phoneNum);
-//                    }
-//                });
                 imageView.setOnLongClickListener(new View.OnLongClickListener() {
                     @Override
                     public boolean onLongClick(View v) {
@@ -261,6 +250,12 @@ public class MainActivity extends Activity {
         ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
         PagerAdapter adapter = new ViewAdapter(pages);
         pager.setAdapter(adapter);
+
+        mytab.setupWithViewPager(pager);
+
+        for (int i = 0; i < mTabTitles.size(); i++) {
+            mytab.getTabAt(i).setText(mTabTitles.get(i));
+        }
     }
 
     private Integer getViewTag(View v) {
